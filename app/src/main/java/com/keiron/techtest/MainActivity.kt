55c79962ef7aad9a3596f1321f9ea493c4f846f3
Gulrajani, Keiron (Developer) <keiron.gulrajani@sky.uk>
2019-04-26 11:44:28 +0100

    Added views for main/details screens

--- app/src/main/java/com/keiron/techtest/MainActivity.kt
+++ app/src/main/java/com/keiron/techtest/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.keiron.techtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.keiron.techtest.section.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragmentWith(true, MainFragment.newInstance())
+    }
+
+    private fun replaceFragmentWith(isRoot: Boolean, fragment: Fragment) {
+        if (isRoot) {
+            for (i in 0 until supportFragmentManager.backStackEntryCount) {
+                supportFragmentManager.popBackStack()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.parent, fragment)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.parent, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
